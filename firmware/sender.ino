1a1a48edabb127cd7664555dad272bc0a077f863
shaahid1 <shaahid@gmail.com>
2015-12-11 16:19:01 -0600

    Create sender.ino

---
+++ firmware/sender.ino
@@ -0,0 +1,60 @@
+/*
+   Dec 2014 - TMRh20 - Updated
+   Derived from examples by J. Coliz <maniacbug@ymail.com>
+*/
+/**
+ * Example for efficient call-response using ack-payloads
+ *
+ * This example continues to make use of all the normal functionality of the radios including
+ * the auto-ack and auto-retry features, but allows ack-payloads to be written optionlly as well.
+ * This allows very fast call-response communication, with the responding radio never having to
+ * switch out of Primary Receiver mode to send back a payload, but having the option to switch to
+ * primary transmitter if wanting to initiate communication instead of respond to a commmunication.
+ */
+
+#include "particle-rf24/particle-rf24.h"
+
+//
+// Hardware configuration
+// from https://github.com/mshoemaker/SparkCore-RF24
+
+/*
+  PINOUTS
+  http://docs.spark.io/#/firmware/communication-spi
+  http://maniacbug.wordpress.com/2011/11/02/getting-started-rf24/
+
+  SPARK CORE    SHIELD SHIELD    NRF24L01+
+  GND           GND              1 (GND)
+  3V3 (3.3V)    3.3V             2 (3V3)
+  D6 (CSN)      9  (D6)          3 (CE)
+  A2 (SS)       10 (SS)          4 (CSN)
+  A3 (SCK)      13 (SCK)         5 (SCK)
+  A5 (MOSI)     11 (MOSI)        6 (MOSI)
+  A4 (MISO)     12 (MISO)        7 (MISO)
+
+  NOTE: Also place a 10-100uF cap across the power inputs of
+        the NRF24L01+.  I/O o fthe NRF24 is 5V tolerant, but
+        do NOT connect more than 3.3V to pin 1!!!
+ */
+
+/****************** User Config ***************************/
+/***      Set this radio as radio number 0 or 1         ***/
+bool radioNumber = 0;
+
+/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins D6 & A2 */
+RF24 radio(D6,A2);
+/**********************************************************/
+                                                                           // Topology
+byte addresses[][6] = {"1Node","2Node"};              // Radio pipe addresses for the 2 nodes to communicate.
+
+void setup(){
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(pipe);}
+ 
+void loop(){
+  for (int x=0;x<255;x++){
+    msg[0] = x;
+    radio.write(msg, 1);
+  }
+}
